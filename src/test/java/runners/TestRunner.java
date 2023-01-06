dd7523e524ff7ef0f3adc4b3a621a2b057993d3b
mg7890 <mutlu.gunduz@yahoo.com>
2023-01-05 19:45:45 -0500

    update

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 				 features="./src/test/resources/features", glue="step_definitions",
 				 dryRun=false, 
 				 //when its true, cucumber only runs the scenario steps that have not been implemented)
-				 tags= "@userManagement",  
+				 tags= "@amazonSearchTest",  
 				 publish=true 
 				 )
 				 		
